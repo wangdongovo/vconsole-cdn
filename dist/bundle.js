5a337043a51c9013d0e9eb3f1a385e164c834d87
Martina Emerson <wangdongovo@gmail.com>
2024-02-29 17:51:09 +0800

    fix: /

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,107 @@
-!function(e,o){"object"==typeof exports&&"object"==typeof module?module.exports=o():"function"==typeof define&&define.amd?define([],o):"object"==typeof exports?exports.vconsole=o():e.vconsole=o()}(this,(()=>(()=>{"use strict";var e={};return{762:function(e,o){var r=this&&this.__spreadArray||function(e,o,r){if(r||2===arguments.length)for(var t,n=0,a=o.length;n<a;n++)!t&&n in o||(t||(t=Array.prototype.slice.call(o,0,n)),t[n]=o[n]);return e.concat(t||Array.prototype.slice.call(o))};Object.defineProperty(o,"__esModule",{value:!0}),o.success=o.error=o.warn=o.log=void 0;var t={};[{name:"log",config:{backgroundGradient:"linear-gradient(to right, #3498DB, #2C3E50)",messageStyle:"padding: 1px;",highlight:"#3498DB"}},{name:"warn",config:{backgroundGradient:"linear-gradient(to right, #F39C12, #D35400)",messageStyle:"padding: 1px;",highlight:"#F39C12"}},{name:"error",config:{backgroundGradient:"linear-gradient(to right, #E74C3C, #C0392B)",messageStyle:"padding: 1px;",highlight:"#E74C3C"}},{name:"success",config:{backgroundGradient:"linear-gradient(to right, #2ECC71, #27AE60)",messageStyle:"padding: 1px;",highlight:"#2ECC71"}}].forEach((function(e){var o=e.name,n=e.config;t[o]=function(){for(var e=[],o=0;o<arguments.length;o++)e[o]=arguments[o];var t=(new Date).toISOString().replace("T"," ").slice(0,-5);console.log.apply(console,r(["%c".concat(t,"%c ").concat(e.shift()," "),"background: ".concat(n.backgroundGradient,"; color: white; ").concat(n.messageStyle,"; padding: 0.3em; border-radius: 0.3em;"),"".concat(n.messageStyle,"color: ").concat(n.highlight,"; font-weight: bold; font-size: 1.2em;")],e,!1))}})),o.log=t.log,o.warn=t.warn,o.error=t.error,o.success=t.success}}[762](0,e),e})()));
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["vconsole"] = factory();
+	else
+		root["vconsole"] = factory();
+})(this, () => {
+return /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 762:
+/***/ (function(__unused_webpack_module, exports) {
+
+
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.success = exports.error = exports.warn = exports.log = void 0;
+// 定义一个空的 vconsoles 对象，用于存储 console 方法
+var vconsoles = {};
+// 创建函数 createVConsole，用于生成 console 方法
+var createVConsole = function (consoleTypes) {
+    consoleTypes.forEach(function (_a) {
+        var name = _a.name, config = _a.config;
+        vconsoles[name] = function () {
+            var args = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                args[_i] = arguments[_i];
+            }
+            var currentDate = new Date();
+            var currentTime = currentDate.toISOString().replace('T', ' ').slice(0, -5);
+            console.log.apply(console, __spreadArray(["%c".concat(currentTime, "%c ").concat(args.shift(), " "), "background: ".concat(config.backgroundGradient, "; color: white; ").concat(config.messageStyle, "; padding: 0.3em; border-radius: 0.3em;"), // 渐变背景色
+                "".concat(config.messageStyle, "color: ").concat(config.highlight, "; font-weight: bold; font-size: 1.2em;")], args // 其余参数保持不变
+            , false));
+        };
+    });
+};
+// 定义 consoleTypes 数组，包含各种控制台方法的样式信息
+var consoleTypes = [
+    {
+        name: 'log',
+        config: {
+            backgroundGradient: 'linear-gradient(to right, #3498DB, #2C3E50)', // 深蓝色
+            messageStyle: 'padding: 1px;', // 调整文字样式
+            highlight: '#3498DB' // 深蓝色
+        }
+    },
+    {
+        name: 'warn',
+        config: {
+            backgroundGradient: 'linear-gradient(to right, #F39C12, #D35400)', // 橙色渐变
+            messageStyle: 'padding: 1px;', // 调整文字样式
+            highlight: '#F39C12' // 橙色
+        }
+    },
+    {
+        name: 'error',
+        config: {
+            backgroundGradient: 'linear-gradient(to right, #E74C3C, #C0392B)', // 红色渐变
+            messageStyle: 'padding: 1px;', // 调整文字样式
+            highlight: '#E74C3C' // 红色
+        }
+    },
+    {
+        name: 'success',
+        config: {
+            backgroundGradient: 'linear-gradient(to right, #2ECC71, #27AE60)', // 绿色渐变
+            messageStyle: 'padding: 1px;', // 调整文字样式
+            highlight: '#2ECC71' // 绿色
+        }
+    }
+];
+// 调用 createVConsole 函数，根据 consoleTypes 生成对应的控制台方法
+createVConsole(consoleTypes);
+// 导出每个控制台方法
+exports.log = vconsoles.log;
+exports.warn = vconsoles.warn;
+exports.error = vconsoles.error;
+exports.success = vconsoles.success;
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__[762](0, __webpack_exports__);
+/******/ 	
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,49 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
-module.exports = {
-  mode: 'production', // 或者 'development'
-  entry: './src/vconsole/index.ts', // 入口文件
-  output: {
-    filename: 'bundle.js', // 输出文件名
-    path: path.resolve(__dirname, 'dist'), // 输出目录
-    library: 'vconsole', // 将 vconsole 作为全局变量导出
-    libraryTarget: 'umd', // 设置打包目标为 UMD 格式
-    globalObject: 'this' // 兼容不同环境下的全局对象
-  },
-  resolve: {
-    extensions: ['.ts', '.js'], // 支持的文件扩展名
-    alias: {
-      '@': path.resolve(__dirname, 'src') // 设置模块别名
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/, // 匹配以 .ts 结尾的文件
-        use: 'ts-loader', // 使用 ts-loader 来处理 TypeScript 文件
-        exclude: /node_modules/ // 排除 node_modules 目录
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production'
+
+  return {
+    mode: argv.mode, // 通过命令行参数传递模式
+    entry: './src/vconsole/index.ts', // 入口文件
+    output: {
+      filename: isProduction ? '[name].[contenthash].js' : 'bundle.js', // 输出文件名
+      path: path.resolve(__dirname, 'dist'), // 输出目录
+      library: 'vconsole', // 将 vconsole 作为全局变量导出
+      libraryTarget: 'umd', // 设置打包目标为 UMD 格式
+      globalObject: 'this' // 兼容不同环境下的全局对象
+    },
+    resolve: {
+      extensions: ['.ts', '.js'], // 支持的文件扩展名
+      alias: {
+        '@': path.resolve(__dirname, 'src') // 设置模块别名
       }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(), // 清理输出目录
-    new HtmlWebpackPlugin({
-      // 生成 HTML 文件
-      template: './index.html', // 指定 HTML 模板文件
-      filename: 'index.html', // 输出文件名
-      inject: 'body' // 将 bundle.js 注入到 body 标签底部
-    })
-  ]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/, // 匹配以 .ts 结尾的文件
+          use: 'ts-loader', // 使用 ts-loader 来处理 TypeScript 文件
+          exclude: /node_modules/ // 排除 node_modules 目录
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(), // 清理输出目录
+      new HtmlWebpackPlugin({
+        template: './index.html', // 指定 HTML 模板文件
+        filename: 'index.html', // 输出文件名
+        inject: 'body' // 将 bundle.js 注入到 body 标签底部
+      })
+    ],
+    optimization: {
+      minimize: isProduction, // 仅在生产环境下进行代码压缩
+      minimizer: [
+        new TerserPlugin() // 使用 TerserPlugin 进行代码压缩
+      ]
+    }
+  }
 }
